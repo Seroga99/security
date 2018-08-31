9d67a942becf6908574cd37f79d6315515a2416c
Александр Ступак <aleksandrstupak@MacBook-Air-Aleksandr.local>
2018-08-31 23:36:59 +0300

    add gallery

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
 import Footer from './Components/Footer/Footer';
 import Header from './Components/Header/Header';
+import Gallery from './Components/Gallery/Gallery';
 
 class App extends Component {
+
+  state = {
+    imageGallery: ['https://placeimg.com/1000/900/any', 'https://placeimg.com/1000/900/animal', 'https://placeimg.com/1000/800/any', 'https://placeimg.com/1000/900/any']
+  }
+
   render() {
+    const {imageGallery} = this.state;
     return (
       <div>
         <Header/>
+        <Gallery massImage={imageGallery}/>
         <Footer/>
       </div>
     );
---
+++ src/Components/Gallery/Gallery.jsx
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import './Gallery.css';
+import ImageGallery from 'react-image-gallery';
+
+class Gallery extends Component {
+    
+    render() {
+        const images = [
+            {
+              original: 'https://placeimg.com/840/500/any',
+              thumbnail: 'https://placeimg.com/450/250/any',
+            },
+            {
+              original: 'https://placeimg.com/840/500/any',
+              thumbnail: 'https://placeimg.com/550/350/any'
+            },
+            {
+              original: 'https://placeimg.com/840/500/any',
+              thumbnail: 'https://placeimg.com/550/350/any',
+              
+            },
+            {
+                original: 'https://placeimg.com/840/500/any',
+                thumbnail: 'https://placeimg.com/550/350/any',
+                
+              },
+              {
+                original: 'https://placeimg.com/840/500/any',
+                thumbnail: 'https://placeimg.com/550/350/any',
+                
+              },
+            
+          ]
+        return (
+            <div className='container'>
+<ImageGallery items={images} autoPlay={true} showBullets={true} showIndex={true} />
+                {/* {massImage.map(el => <ul>
+
+                    <li><img src={el} alt=""/></li>
+                    
+                    </ul>)} */}
+            </div>
+        );
+    }
+}
+
+Gallery.propTypes = {
+
+};
+
+export default Gallery;
